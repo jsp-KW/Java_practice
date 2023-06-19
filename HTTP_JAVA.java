7319ca54358447ee90fb9d94240090d2a58d09fe
jsp-KW <70892401+jsp-KW@users.noreply.github.com>
2023-06-19 10:56:58 +0900

    Update HTTP_JAVA.java

--- HTTP_JAVA.java
+++ HTTP_JAVA.java
@@ -15,7 +15,7 @@ public class HTTP_JAVA {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		int PORT =  8081; 
+		int PORT =  8080; 
 		
 		ServerSocket serverSocket = new ServerSocket(PORT);
 		System.out.println("서버가 시작됩니다. 포트번호: " + PORT);
@@ -31,17 +31,16 @@ public class HTTP_JAVA {
 			BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
 			
 			if (request.startsWith("GET")) {
-			    String response = "HTTP/1.1 200 OK\r\n";
-                response += "Content-Type: text/html; charset=utf-8\r\n\r\n"; // 인코딩 정보를 포함한 Content-Type 헤더
-                response += "<html><body><h1>GET방식으로 요청을 확인했습니다.</h1></body></html>"; // 응답으로 보낼 내용 (HTML 형식)
-                wr.write(response);
-                wr.flush();
+				String response = "HTTP/1.1 200 OK\r\n";
+		            	response += "Content-Type: text/html; charset=utf-8\r\n\r\n"; // 인코딩 정보를 포함한 Content-Type 헤더
+		                response += "<html><body><h1>GET방식으로 요청을 확인했습니다.</h1></body></html>"; // 응답으로 보낼 내용 (HTML 형식)
+		                wr.write(response);
+		                wr.flush();
 			}else {
 				String response ="HTTP/1.1  400 Bad Request\r\n\r\n";
 				wr.write(response);
 				wr.flush();
 			}
-			
 			
 			wr.close();
 			br.close();
